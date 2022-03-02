875d74ef549df1f5a6158a3336cc3adba35825b2
theshubhamgour <iamtheshubhamgour@gmail.com>
2022-03-02 15:41:51 +0530

    commit

---
+++ 19#functionoverloading.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    cout << "Sum function 2 with argument" << endl;
+    return a + b;
+}
+
+int sum(int a, int b, int c)
+{
+    cout << "Sum function 3 with argument" << endl;
+    return a + b + c;
+}
+
+// Calculate the volume of a cylinder
+int volume(double r, int h)
+{
+    return (3.14 * r * r * h);
+}
+
+// Calculate the volume of a cube
+int volume(int a)
+{
+    return (a * a * a);
+}
+
+// Rectangular box
+int volume(int l, int b, int h)
+{
+    return (l * b * h);
+}
+
+int main()
+{
+    cout << "The sum of (3,6) is " << sum(3, 6) << endl;
+    cout << "The sum of (3,6,7) is " << sum(3, 6, 7) << endl;
+    cout << "The volume of cuboid of 3, 7 and 6 is " << volume(3, 7, 6) << endl;
+    cout << "The volume of cylinder of radius 3 and height 6 is " << volume(3, 6) << endl;
+    cout << "The volume of cube of side 3 is " << volume(3) << endl;
+}
